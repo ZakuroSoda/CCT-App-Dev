a3fd8904b1547f262c62fec1984774400593f101
ZakuroSoda <zakurosoda@gmail.com>
2022-07-13 16:41:29 +0800

    Poop

--- html/src/index.js
+++ html/src/index.js
@@ -1,4 +1,20 @@
+// width="800px" height="500px"
+//Setup
 let canvas = document.getElementById("gameScreen");
 let ctx = canvas.getContext("2d");
-ctx.fillStyle = "#ff0000"
-ctx.fillRect(350,100,100,100)
+ctx.clearRect(0,0,800,500);
+ctx.fillStyle = '#F000000';
+
+ctx.fillRect(100,100,100,100);
+let xCoordinate = 100;
+let yCoordinate = 100;
+function movePaddleRight(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillRect(xCoordinate+100,100,100,100);
+}
+
+document.addEventListener("keypress", function(event) {
+    if (event.keyCode == 13) {
+      movePaddleRight();
+    }
+  });
