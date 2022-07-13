7f4d6ea8cb6462c38a74f32c1480c53c281d7999
Reyes Lee <87415308+ZakuroSoda@users.noreply.github.com>
2022-07-13 17:46:20 +0800

    Update index.js

--- html/src/index.js
+++ html/src/index.js
@@ -5,16 +5,42 @@ let ctx = canvas.getContext("2d");
 ctx.clearRect(0,0,800,500);
 ctx.fillStyle = '#F000000';
 
-ctx.fillRect(100,100,100,100);
-let xCoordinate = 100;
-let yCoordinate = 100;
+var centerPointWidth = 50;
+var centerPointHeight = 50;
+ctx.fillRect((canvas.width / 2) - (centerPointWidth / 2), (canvas.height / 2) - (centerPointHeight / 2), centerPointWidth, centerPointHeight);
+let xCoordinate = (canvas.width / 2) - (centerPointWidth / 2);
+let yCoordinate = (canvas.height / 2) - (centerPointHeight / 2);
+
 function movePaddleRight(){
+    //Check for border collision
+    if (xCoordinate+50 >= 800){
+      ;
+    }
+    else {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.fillRect(xCoordinate+50,yCoordinate,centerPointWidth,centerPointHeight);
+      xCoordinate += 50;
+    }
+}
+function movePaddleLeft(){
+  //Check for border collision
+  if (xCoordinate-50 <= -50){
+    ;
+  }
+  else {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillRect(xCoordinate+100,100,100,100);
+    ctx.fillRect(xCoordinate-50,yCoordinate,centerPointWidth,centerPointHeight);
+      xCoordinate -= 50;
+  }
 }
 
 document.addEventListener("keypress", function(event) {
-    if (event.keyCode == 13) {
-      movePaddleRight();
+  if (event.code == "KeyD") {
+    movePaddleRight();
+  }
+}, true);
+document.addEventListener("keypress", function(event) {
+    if (event.code == "KeyA") {
+      movePaddleLeft();
     }
-  });
+}, true);
