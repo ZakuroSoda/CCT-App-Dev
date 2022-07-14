5053f585ab18d688e0c642084b68269d1f37c399
ZakuroSoda <zakurosoda@gmail.com>
2022-07-14 09:42:37 +0800

    commit

--- html/src/index.js
+++ html/src/index.js
@@ -1,16 +1,29 @@
 // width="800px" height="500px"
 //Setup
-let canvas = document.getElementById("gameScreen");
-let ctx = canvas.getContext("2d");
+var canvas = document.getElementById("gameScreen");
+var ctx = canvas.getContext("2d");
+ctx.font = "30px Arial";
+ctx.fillText("Hello World", 100, 100);
+
 ctx.clearRect(0,0,800,500);
 ctx.fillStyle = '#F000000';
 
+var score = 0;
 var centerPointWidth = 50;
 var centerPointHeight = 50;
 ctx.fillRect((canvas.width / 2) - (centerPointWidth / 2), (canvas.height / 2) - (centerPointHeight / 2), centerPointWidth, centerPointHeight);
-let xCoordinate = (canvas.width / 2) - (centerPointWidth / 2);
-let yCoordinate = (canvas.height / 2) - (centerPointHeight / 2);
+var xCoordinate = (canvas.width / 2) - (centerPointWidth / 2);
+var yCoordinate = (canvas.height / 2) - (centerPointHeight / 2);
 
+
+function updateScore(score){
+    if (score == undefined){
+      console.log("fk u cheater");
+    }
+    else {
+      document.getElementById("score").innerHTML = "Score: " + score;
+    }
+}
 function movePaddleRight(){
     //Check for border collision
     if (xCoordinate+50 >= 750){
@@ -20,6 +33,8 @@ function movePaddleRight(){
       ctx.clearRect(0, 0, canvas.width, canvas.height);
       ctx.fillRect(xCoordinate+50,yCoordinate,centerPointWidth,centerPointHeight);
       xCoordinate += 50;
+      score += 1;
+      updateScore(score);
     }
 }
 function movePaddleLeft(){
@@ -30,7 +45,9 @@ function movePaddleLeft(){
   else {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillRect(xCoordinate-50,yCoordinate,centerPointWidth,centerPointHeight);
-      xCoordinate -= 50;
+    xCoordinate -= 50;
+    score += 1;
+    updateScore(score);
   }
 }
 function movePaddleUp(){
@@ -41,6 +58,8 @@ function movePaddleUp(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillRect(xCoordinate,yCoordinate-50,centerPointWidth,centerPointHeight);
     yCoordinate -= 50;
+    score += 1;
+    updateScore(score);
   }
 }
 function movePaddleDown(){
@@ -51,26 +70,29 @@ function movePaddleDown(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillRect(xCoordinate,yCoordinate+50,centerPointWidth,centerPointHeight);
     yCoordinate += 50;
+    score += 1;
+    updateScore(score);
   }
 }
 
 document.addEventListener("keypress", function(event) {
-  if (event.code == "KeyD") {
+  if (event.code == "KeyD" || event.keycode == "ArrowRight") {
     movePaddleRight();
   }
 }, true);
 document.addEventListener("keypress", function(event) {
-    if (event.code == "KeyA") {
+    if (event.code == "KeyA" || event.code == "ArrowLeft") {
       movePaddleLeft();
     }
 }, true);
 document.addEventListener("keypress", function(event) {
-  if (event.code == "KeyW") {
+  if (event.code == "KeyW" || event.code == "ArrowUp") {
     movePaddleUp();
   }
 }, true);
 document.addEventListener("keypress", function(event) {
-  if (event.code == "KeyS") {
+  if (event.code == "KeyS" || event.code == "ArrowDown") {
     movePaddleDown();
   }
 }, true);
+
