06c2c61e7563b3f7b9eb6c747873af214141173f
ZakuroSoda <zakurosoda@gmail.com>
2022-07-14 09:59:05 +0800

    abc

--- html/src/index.js
+++ html/src/index.js
@@ -2,8 +2,6 @@
 //Setup
 var canvas = document.getElementById("gameScreen");
 var ctx = canvas.getContext("2d");
-ctx.font = "30px Arial";
-ctx.fillText("Hello World", 100, 100);
 
 ctx.clearRect(0,0,800,500);
 ctx.fillStyle = '#F000000';
@@ -16,6 +14,17 @@ var xCoordinate = (canvas.width / 2) - (centerPointWidth / 2);
 var yCoordinate = (canvas.height / 2) - (centerPointHeight / 2);
 
 
+function randomRange(min, max) {
+  return Math.floor((Math.random() * (max - min + 1)) + min);
+}
+function spawnFruit(){
+  ctx.fillStyle = '#00FF00';
+  var xCoordinateFruit = randomRange(0,770);
+  var yCoordinateFruit = randomRange(0,470);
+  ctx.fillRect(xCoordinateFruit,yCoordinateFruit,30,30);
+}
+spawnFruit()
+ctx.fillStyle = '#F000000';
 function updateScore(score){
     if (score == undefined){
       console.log("fk u cheater");
@@ -74,25 +83,27 @@ function movePaddleDown(){
     updateScore(score);
   }
 }
-
-document.addEventListener("keypress", function(event) {
-  if (event.code == "KeyD" || event.keycode == "ArrowRight") {
-    movePaddleRight();
-  }
-}, true);
-document.addEventListener("keypress", function(event) {
-    if (event.code == "KeyA" || event.code == "ArrowLeft") {
-      movePaddleLeft();
+function controls(){
+  document.addEventListener("keypress", function(event) {
+    if (event.code == "KeyD" || event.keycode == "ArrowRight") {
+      movePaddleRight();
     }
-}, true);
-document.addEventListener("keypress", function(event) {
-  if (event.code == "KeyW" || event.code == "ArrowUp") {
-    movePaddleUp();
-  }
-}, true);
-document.addEventListener("keypress", function(event) {
-  if (event.code == "KeyS" || event.code == "ArrowDown") {
-    movePaddleDown();
-  }
-}, true);
+  }, true);
+  document.addEventListener("keypress", function(event) {
+      if (event.code == "KeyA" || event.code == "ArrowLeft") {
+        movePaddleLeft();
+      }
+  }, true);
+  document.addEventListener("keypress", function(event) {
+    if (event.code == "KeyW" || event.code == "ArrowUp") {
+      movePaddleUp();
+    }
+  }, true);
+  document.addEventListener("keypress", function(event) {
+    if (event.code == "KeyS" || event.code == "ArrowDown") {
+      movePaddleDown();
+    }
+  }, true);
+}
 
+controls()
